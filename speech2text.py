03274aa6be4350ded196912bfdc8fe1ba63eaf01
smith076 <37410262+smith076@users.noreply.github.com>
2020-11-08 14:25:51 +0530

    first commit

---
+++ speech2text.py
@@ -0,0 +1,9 @@
+import win32com.client as wincl
+speak = wincl.Dispatch("SAPI.SpVoice")
+val=input("Enter a name")
+speak.Speak("Hello "+val+ ". I am David. I am your humble assistant.")
+#speak.Speak("Enter 2 numbers")
+#a=input(". . .")
+#b=input(". . .")
+#sum=a+b
+#speak.Speak("Sum is "+sum+"")
